469e07b29d47aeab9c89b90a44c7dd85be378dfb
sakitam21 <1983409794@qq.com>
2021-08-27 17:26:27 +0800

    tabBar

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,30 @@
 export default {
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/person_center/person_center'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    color: '#cdcdcd',
+    selectedColor: '#169891',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页',
+        iconPath: 'static/images/tabbar/home.png',
+        selectedIconPath: 'static/images/tabbar/home_selected.png'
+      },
+      {
+        pagePath: 'pages/person_center/person_center',
+        text: '我的',
+        iconPath: 'static/images/tabbar/person_center.png',
+        selectedIconPath: 'static/images/tabbar/person_center_selected.png'
+      }
+    ]
   }
 }
---
+++ src/pages/person_center/person_center.tsx
@@ -0,0 +1,24 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './person_center.scss'
+
+export default class PersonCenter extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='person_center'>
+        <Text>个人中心</Text>
+      </View>
+    )
+  }
+}
